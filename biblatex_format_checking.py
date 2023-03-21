01daf282478f4eb160e5605c7bb502ae6926caf2
Xinhe <aspasia.yu@gmail.com>
2023-03-21 10:56:51 +0100

    add counting system

--- biblatex_format_checking.py
+++ biblatex_format_checking.py
@@ -16,13 +16,11 @@ except:
         print("UTF-8 encoding")
     except:
         print("File is neither encoding in mbcs nor in UTF-8")
-        print("Please enter your encoding system:")
-        style = input()
-        file = open(path, encoding=style)
 print("---- Parsing ----")
 
 i = 1
 counter = 0
+warning = 0
 for line in file:
     raw_line = line
     if line.startswith("@"):
@@ -32,6 +30,7 @@ for line in file:
         except TypeError:
             filename = line.split("{")[1][0:-2]
             print("Not recommended bib reference name: ", filename)
+            warning += 1
     elif line == "}\n":
         if isListAll == True:
             print("Checked: " + filename)
@@ -40,6 +39,7 @@ for line in file:
     elif i == 1:
         if line == "\n":
             print(filename + " is not closed with a '}' or has an empty line inside the curly brackets.")
+            warning += 1
             i = 0 #quit the entry but maybe has other syntactic errors
             continue
         line = line.split("{")
@@ -47,8 +47,12 @@ for line in file:
             for i in re.finditer(r"&", line[-1]):
                 if line[-1][i.start() - 1] != '\\':
                     print("'&' instead of '\&': " + filename + " | " + raw_line)
+                    warning += 1
         if (not line[-1].endswith("},\n")) and (not line[-1].endswith('",\n')):
             print("Last caracter is not comma: " + filename + " | " + raw_line)
+            warning += 1
 
 file.close()
 print("Total entries checked: " + str(counter))
+if warning:
+    print("Total number of warnings: " + str(warning))
